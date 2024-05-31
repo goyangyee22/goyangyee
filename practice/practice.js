a38ea63d7fa6b357d5067bb6d53103ee33b19b96
LeeJh <4447447@naver.com>
2024-05-31 17:31:41 +0900

    Update practice.js

--- practice/practice.js
+++ practice/practice.js
@@ -1,15 +1,15 @@
 const boxes = document.querySelectorAll(".box");
 const imgBox = document.querySelector(".imageBox")
 
-boxes.forEach((box) => {
-    box.addEventListener("dragover", (e) => {
+boxes.forEach((el) => {
+    el.addEventListener("dragover", (e) => {
         e.preventDefault();
         e.currentTarget.classList.add("hovered");
     });
-    box.addEventListener("dragleave", (e) => {
+    el.addEventListener("dragleave", (e) => {
         e.currentTarget.classList.remove("hovered");
     })
-    box.addEventListener("drop", (e) => {
+    el.addEventListener("drop", (e) => {
         e.currentTarget.classList.remove("hovered");
         e.currentTarget.append(imgBox);
     })
