83b31a14d1eea30248ae546c573d99e58032f43a
yooeuddeum <48862667+goyangyee22@users.noreply.github.com>
2024-07-01 18:15:45 +0900

    update

--- react/02-rsp_game/src/App.js
+++ react/02-rsp_game/src/App.js
@@ -1,3 +1,4 @@
+import Board from "./Board";
 import "./App.css";
 import logo from "./assets/logo.png";
 
@@ -7,8 +8,10 @@ function random(n) {
 
 function App() {
   const [gameHistory, setGameHistory] = useState([]);
+  const [otherGameHistory, setOtherGameHistory] = userState([]);
   console.log(gameHistory);
-  console.log(setGameHistory);
+  console.log(otherGameHistory);
+
   const handleRollClick = () => {
     // 바위 = 1, 가위 = 2, 보 = 3이라고 가정하고 숫자를 뽑습니다.
     const myNum = [];
@@ -16,20 +19,49 @@ function App() {
 
     // 기록을 추가합니다.
     setGameHistory([...gameHistory, myNum]);
+    setOtherGameHistory([...otherGameHistory, otherNum]);
   };
 
   const handleClearClick = () => {
     setGameHistory([]);
+    setOtherGameHistory([]);
   };
   return (
     <div className="App">
       <div>
         <img src={logo} className="App-logo" />
         <h1 className="App-title">가위바위보</h1>
-        <button
-          className="App-button yellow"
-          onClick={handleClearClick}
-        ></button>
+        <button className="App-button yellow" onClick={handleClearClick}>
+          초기화
+        </button>
+      </div>
+      <div className="App-boards">
+        <Board name="나" gameHistory={gameHistory} />
+        <Board name="상대" gameHistroy={otherGameHistory} />
+      </div>
+      <div className="App-divide">
+        배점
+        <select className="App-select">
+          <option value="1"></option>
+          <option value="2"></option>
+          <option value="3"></option>
+          <option value="4"></option>
+          <option value="5"></option>
+        </select>
+      </div>
+      <div className="App-result">
+        <Board />
+      </div>
+      <div>
+        <div>
+          <img src="../src/assets/rock.svg" />
+        </div>
+        <div>
+          <img src="../src/assets/scissor.svg" />
+        </div>
+        <div>
+          <img src="../src/assets/paper.svg" />
+        </div>
       </div>
     </div>
   );
---
+++ react/02-rsp_game/src/Board.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Board({ name, result, gameHistory }) {
+  return (
+    <div className="App-board">
+      <h2>{name}</h2>
+    </div>
+  );
+}
+
+export default Board;
