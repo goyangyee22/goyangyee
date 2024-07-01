0f18f480da1353f73dbaf2719a9c60d5fdd3531b
yooeuddeum <48862667+goyangyee22@users.noreply.github.com>
2024-07-01 17:41:40 +0900

    Update App.js

--- react/02-rsp_game/src/App.js
+++ react/02-rsp_game/src/App.js
@@ -1,7 +1,38 @@
 import "./App.css";
+import logo from "./assets/logo.png";
+
+function random(n) {
+  return Math.ceil(Math.random * n);
+}
 
 function App() {
-  return <div className="App"></div>;
+  const [gameHistory, setGameHistory] = useState([]);
+  console.log(gameHistory);
+  console.log(setGameHistory);
+  const handleRollClick = () => {
+    // 바위 = 1, 가위 = 2, 보 = 3이라고 가정하고 숫자를 뽑습니다.
+    const myNum = [];
+    const otherNum = random(3);
+
+    // 기록을 추가합니다.
+    setGameHistory([...gameHistory, myNum]);
+  };
+
+  const handleClearClick = () => {
+    setGameHistory([]);
+  };
+  return (
+    <div className="App">
+      <div>
+        <img src={logo} className="App-logo" />
+        <h1 className="App-title">가위바위보</h1>
+        <button
+          className="App-button yellow"
+          onClick={handleClearClick}
+        ></button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
