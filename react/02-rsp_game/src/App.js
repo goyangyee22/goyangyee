b777ae25b4ced0629c88c52e3b0f49e9b9098764
yooeuddeum <48862667+goyangyee22@users.noreply.github.com>
2024-07-02 09:42:41 +0900

    update

--- react/02-rsp_game/src/App.js
+++ react/02-rsp_game/src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import reset from "./assets/ic-reset.svg";
 
 function App() {
-  return <div className="App">Hello, React!!</div>;
+  return (
+    <div className="App">
+      <h1 className="App-heading">가위바위보</h1>
+      <img className="App-reset" src={reset} />
+      <div className="App-scores"></div>
+      <div className="Box App-box"></div>
+      <div></div>
+    </div>
+  );
 }
 
 export default App;
