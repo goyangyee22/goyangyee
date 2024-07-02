c15d06f9ff7c63a4929996ad390539ebc25ac3b8
yooeuddeum <48862667+goyangyee22@users.noreply.github.com>
2024-07-02 15:13:19 +0900

    update

--- react/02-rsp_game/src/HandButton.js
+++ react/02-rsp_game/src/HandButton.js
@@ -3,8 +3,11 @@ import HandIcon from "./HandIcon";
 import "./HandButton.css";
 
 function HandButton({ value, onClick }) {
+  const handleClick = () => {
+    onClick(value);
+  };
   return (
-    <button className="HandButton" onClick={onClick}>
+    <button className="HandButton" onClick={handleClick}>
       <HandIcon className="HandButton-icon" value={value} />
     </button>
   );
