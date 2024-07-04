dfa9c0f4c4f97ac75be77f5e0fb8aa7006474c46
yooeuddeum <48862667+goyangyee22@users.noreply.github.com>
2024-07-04 15:53:59 +0900

    update

--- react/03-moviepedia/src/practice/State.js
+++ react/03-moviepedia/src/practice/State.js
@@ -1,10 +1,28 @@
 import React, { useState } from "react";
 
+let value;
+function useState(initialValue) {
+  if (value === undefined) {
+    value = initialValue;
+  }
+  const setState = (newValue) => {
+    value = newValue;
+  };
+  return [value, setState];
+}
+
 function State(props) {
   const [state, setState] = useState(0);
 
   const onClickHandler = () => {
     console.log("click!!");
+
+    // setState는 값을 바꾸는 게 주목적이 아닌 화면을 재 렌더링 하는 게 주 목적
+    setState(1);
+
+    if (state === 1) {
+      console.log("실행될까??");
+    }
   };
   return <h1 onClick={onClickHandler}>State 테스트</h1>;
 }
