59247071252ec5c5087534358f6152203e075f95
yooeuddeum <48862667+goyangyee22@users.noreply.github.com>
2024-07-05 09:49:31 +0900

    update

--- react/03-moviepedia/src/Rating.js
+++ react/03-moviepedia/src/Rating.js
@@ -1,20 +1,31 @@
 import React from "react";
+import "./Rating.css";
 
 const RATINGS = [1, 2, 3, 4, 5];
 
-function Star({ selectRating, rating }) {
+function Star({ selectRating, rating, selected, onHover }) {
   const handleClick = () => {
     selectRating(rating);
   };
 
-  return <span onClick={handleClick}>★</span>;
+  return (
+    <span className="Rating-star" onClick={handleClick} onMouseOver={onHover}>
+      ★
+    </span>
+  );
 }
 
-function Rating({ selectRating }) {
+function Rating({ selectRating, hoverRating, onHover }) {
   return (
     <div>
       {RATINGS.map((arrNum) => (
-        <Star key={arrNum} selectRating={selectRating} rating={arrNum} />
+        <Star
+          key={arrNum}
+          selectRating={selectRating}
+          rating={arrNum}
+          selected={hoverRating}
+          onHover={onHover}
+        />
       ))}
     </div>
     // key 값을 사용하는 이유: 각 요소가 고유하게 식별될 수 있기 위해
--- react/03-moviepedia/src/RatingInput.js
+++ react/03-moviepedia/src/RatingInput.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Rating from "./Rating";
 
 function RatingInput({ inputName, setRating }) {
+  // ratingValue State는 별을 색칠하는 용도의 점수
+  const [ratingValue, setRatingValue] = useState(0);
+
+  // 실제 values.rating을 바꾸는 함수는 setRating
+  // 이 함수를 실행하는 시기는 Star 컴포넌트의 onClick의 시점
   const handleSelect = (nextValue) => {
     setRating(inputName, nextValue);
   };
   return (
     <div>
-      <Rating selectRating={handleSelect} />
+      <Rating
+        selectRating={handleSelect}
+        hoverRating={ratingValue}
+        onHover={setRatingValue}
+      />
     </div>
   );
 }
