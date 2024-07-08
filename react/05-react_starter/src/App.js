6de2ccfe3697f1708718aa97f1e996f85c1ab446
yooeuddeum <48862667+goyangyee22@users.noreply.github.com>
2024-07-08 17:29:48 +0900

    Update App.js

--- react/05-react_starter/src/App.js
+++ react/05-react_starter/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
@@ -20,6 +20,19 @@ function App() {
   // div의 내용을 반환합니다.
   // React에서는 화면의 내용을 변환하고 싶으면 useState()를 활용해야 합니다.
   // useState()는 다시 렌더링 한다는 게 중요
+
+  // useEffect() 최초 렌더링될 때만 실행 됨 [] 때문에
+  useEffect(() => {
+    console.log("나는 화면이 최초 렌더링될 때 실행되는 uef야.");
+  }, []); // [](디펜던시 리스트) 안에는 react가 무엇을 지켜볼 지 작성해준다.
+
+  useEffect(() => {
+    console.log("나는 count가 변경될 때 실행되는 uef야.");
+  }, [num]);
+
+  useEffect(() => {
+    console.log("나는 count가 변경될 때 실행되는 uef야.");
+  }, [text]);
   return (
     <div>
       {/* input에 입력한 값이 console.log에 나옵니다. */}
