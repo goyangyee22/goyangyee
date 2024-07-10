628db0fdfda98032d7076ad00d92a7fdfead620d
yooeuddeum <48862667+goyangyee22@users.noreply.github.com>
2024-07-10 18:58:08 +0900

    Update MBTISelect.js

--- react/06-mbti-color/src/components/MBTISelect.js
+++ react/06-mbti-color/src/components/MBTISelect.js
@@ -12,10 +12,14 @@ const mbtiArr = [
   { mbti: "J", desc: "판단형", groupNum: 3 },
 ];
 
+// 각 MBTI 선택 옵션을 표시합니다.
 function MBTIOption({ option, selected, changeMbti }) {
   const { mbti, desc, groupNum } = option;
   const className = `${styles.mbtiOption} ${selected ? styles.selected : ""}`;
+
+  // MBTI를 선택하면 처리하는 함수입니다.
   const handleMbtiClick = () => {
+    // 선택한 MBTI 유형(groupNum, mbti)을 변경 함수에 전달합니다.
     changeMbti(groupNum, mbti);
   };
   // debugger;
@@ -27,8 +31,11 @@ function MBTIOption({ option, selected, changeMbti }) {
   );
 }
 
+// MBTI 선택 옵션들을 표시하는 부모 컴포넌트입니다.
 function MBTISelect({ mbtiValue, handleChange }) {
+  // MBTI 유형을 변경하는 함수입니다.
   const changeMbti = (selectedGroupNum, selectedMbti) => {
+    // 현재 MBTI와 선택된 MBTI가 다른 경우에 실행됩니다.
     if (mbtiValue[selectedGroupNum] !== selectedMbti) {
       const beforeValue = mbtiValue.slice(0, selectedGroupNum);
       const afterValue = mbtiValue.slice(selectedGroupNum + 1);
@@ -36,6 +43,8 @@ function MBTISelect({ mbtiValue, handleChange }) {
       handleChange(nextValue);
     }
   };
+
+  // mbtiArr 배열을 순회하며 각 MBTI 옵션을 렌더링 합니다.
   return (
     <div className={styles.mbtiOptions}>
       {mbtiArr.map((option, idx) => {
