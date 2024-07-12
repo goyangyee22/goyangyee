b50756cb52a0679411a9451ea340b2b27de38e5f
yooeuddeum <48862667+goyangyee22@users.noreply.github.com>
2024-07-12 16:40:06 +0900

    Update Login.js

--- react/07-styled_componentt/src/components/10/Login.js
+++ react/07-styled_componentt/src/components/10/Login.js
@@ -2,17 +2,17 @@ import React from "react";
 import Button from "./Button";
 import styled from "styled-components";
 import kakao from "./kakao.svg";
+import { Link } from "react-router-dom";
 
 const Container = styled.div`
   width: 400px;
-  margin: 0 auto;
+  margin: 40px auto;
 
   & > h1 {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background: linear-gradient(to right, #ffd8d9, #6750a4);
-    -webkit-background-clip: text;
+    font-family: Pretendard;
+    text-align: center;
+    font-size: 40px;
+    background: linear-gradient(135deg, aqua, purple);
     background-clip: text;
     color: transparent;
   }
@@ -20,11 +20,6 @@ const Container = styled.div`
   & > label {
     align-self: flex-start;
   }
-`;
-
-const Emphasize = styled.div`
-  color: #6750a4;
-  text-decoration: underline;
 `;
 
 const H4Tag = styled.div`
@@ -42,7 +37,7 @@ const KakaoLogin = styled(Button)`
   background-position: 35%;
   border: transparent;
   color: #000;
-  padding-left: 30px;
+  padding-right: 8px;
 
   &:hover {
     background-color: #e4ee05;
@@ -50,20 +45,27 @@ const KakaoLogin = styled(Button)`
 `;
 
 const Input = styled.input`
-  display: inline-block;
-  border-top: none;
-  border-left: none;
-  border-right: none;
+  display: block;
+  border: none;
+  border-bottom: 2px solid #d2d2d2;
   border-color: #d2d2d2;
-  width: 390px;
+  width: 100%;
   font-size: 16px;
-  padding: 8px;
+  padding: 8px 0;
   margin-bottom: 16px;
 
   &:focus {
     border-color: #6750a4;
     outline: none;
   }
+
+  &::placeholder {
+    color: #c4c5cd;
+  }
+`;
+
+const Label = styled.label`
+  color: #e1c6f7;
 `;
 
 function Login(props) {
@@ -71,14 +73,16 @@ function Login(props) {
     <Container>
       <h1>DW 온라인스쿨</h1>
       <H4Tag>
-        회원이 아니신가요? <Emphasize>회원가입 하기</Emphasize>
+        회원이 아니신가요? <Link>회원가입 하기</Link>
       </H4Tag>
-      <label>이메일</label>
-      <Input type="text" placeholder="styled@DW.kr" />
-      <label>비밀번호</label>
-      <Input type="password" placeholder="비밀번호" />
-      <Button>로그인 하기</Button>
-      <KakaoLogin>카카오 로그인</KakaoLogin>
+      <form>
+        <Label htmlFor="email">이메일</Label>
+        <Input type="text" id="email" placeholder="styled@DW.kr" />
+        <Label htmlFor="password">비밀번호</Label>
+        <Input type="password" id="password" placeholder="비밀번호" />
+        <Button>로그인 하기</Button>
+        <KakaoLogin>카카오 로그인</KakaoLogin>
+      </form>
     </Container>
   );
 }
