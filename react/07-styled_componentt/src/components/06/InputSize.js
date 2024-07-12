14db23410dbec41e7cfe3d3d94c4ddf4bb2fea61
yooeuddeum <48862667+goyangyee22@users.noreply.github.com>
2024-07-12 10:42:22 +0900

    Update InputSize.js

--- react/07-styled_componentt/src/components/06/InputSize.js
+++ react/07-styled_componentt/src/components/06/InputSize.js
@@ -11,6 +11,7 @@ const InputSize = styled.input`
   border: ${({ $error }) => ($error ? "2px solid red" : "2px solid black")};
   color: #000;
   padding: 16px;
+  outline: none; // outline: none을 지정하지 않으면 border가 #000이 됨
   font-size: ${({ size }) => SIZES[size] ?? SIZES["medium"]}px;
   border-radius: ${({ $round }) => ($round ? "9999px" : "3px")};
 
