477d04ba9720bede5bc27e5f3049b04c5e35d195
yooeuddeum <48862667+goyangyee22@users.noreply.github.com>
2024-07-15 09:39:54 +0900

    update

--- react/08-dark_theme/src/App.js
+++ react/08-dark_theme/src/App.js
@@ -1,12 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
+import Mainpage from "./pages/Mainpage";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />}>
-          <Route path=".." element={<Home />} />
+        <Route path="/" element={<Home />}>
+          <Route path="" element={<Mainpage />} />
+          <Route path="about" element={<AboutPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
