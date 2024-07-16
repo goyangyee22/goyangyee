989499c83917515297808f3079f0f3c820df97a2
LeeJh <4447447@naver.com>
2024-07-17 06:00:36 +0900

    Update getDateData.js

--- react/08-dark_theme/src/utils/getDateData.js
+++ react/08-dark_theme/src/utils/getDateData.js
@@ -1,5 +1,7 @@
 function getTimes() {
-  const today = new Date();
+  // UTC 표준 시간과 현재 로컬 시간의 차이를 밀리초 단위로 저장합니다.
+  const offset = new Date().getTimezoneOffset()*60000;
+  const today = new Date(Date.now()-offset);
   const isoString = today.toISOString(); //2024-07-15T16:15:24.654z
   const hourMinutes = isoString.split("T")[1].split(":");
   return hourMinutes[0] + hourMinutes[1];
