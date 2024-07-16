0937294401aac929d319289a02e65de1ad853235
yooeuddeum <48862667+goyangyee22@users.noreply.github.com>
2024-07-16 09:32:43 +0900

    Update HomePage.js

--- react/09-dw_online_school/src/pages/HomePage.js
+++ react/09-dw_online_school/src/pages/HomePage.js
@@ -1,7 +1,28 @@
 import React from "react";
+import Container from "../components/Container";
 
 function HomePage(props) {
-  return <div></div>;
+  return (
+    <div>
+      <Container>
+        <div>
+          <h1>
+            코딩이 처음이라면,
+            <br />
+            <strong>DWOS</strong>
+          </h1>
+          <p>
+            11만 명이 넘는 비전공자, 코딩 입문자가 DWOS 무제한 멤버십을
+            선택했어요.
+            <br />
+            지금 함께 시작해보실래요?
+          </p>
+          <div></div>
+        </div>
+        <div></div>
+      </Container>
+    </div>
+  );
 }
 
 export default HomePage;
