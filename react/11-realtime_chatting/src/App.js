3c276d3833c1e991cb0017958a0cc626a60f632b
yooeuddeum <48862667+goyangyee22@users.noreply.github.com>
2024-07-29 14:22:42 +0900

    update

---
+++ react/11-realtime_chatting/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h4>🙏 소원을 빌어주세요</h4>
+        <button>로그아웃</button>
+      </header>
+      <section></section>
+    </div>
+  );
+}
+
+export default App;
