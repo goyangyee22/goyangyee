260fbc310b17273d4d0ab4881bb99f6aa1cc1c3b
yooeuddeum <48862667+goyangyee22@users.noreply.github.com>
2024-08-08 09:22:18 +0900

    update

--- react/14-shop-app/src/App.js
+++ react/14-shop-app/src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { BrowserRouter, Routes } from "react-router-dom";
 
 function App() {
   return (
-   
+    <BrowserRouter>
+      <Routes></Routes>
+    </BrowserRouter>
   );
 }
 
