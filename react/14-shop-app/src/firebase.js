995b6e5d7c32323cd601871586dd5a69484bef63
yooeuddeum <48862667+goyangyee22@users.noreply.github.com>
2024-08-08 17:28:50 +0900

    update

---
+++ react/14-shop-app/src/firebase.js
@@ -0,0 +1,80 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  query,
+  orderBy,
+  limit,
+  where,
+  getDoc,
+  runTransaction,
+  getDocs,
+  doc,
+  updateDoc,
+  deleteDoc,
+} from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBP0BMK34Pmmdwt6Lp_hYIvea2rMCiC5O0",
+  authDomain: "shop-app-30bda.firebaseapp.com",
+  projectId: "shop-app-30bda",
+  storageBucket: "shop-app-30bda.appspot.com",
+  messagingSenderId: "1083407781508",
+  appId: "1:1083407781508:web:d766972066a2054a773cd1",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+function getCollection(collectionName) {
+  return collection(db, collectionName);
+}
+
+function getUserAuth() {
+  return auth;
+}
+
+async function getLastNum(collectionName, field) {
+  const q = query(
+    collection(db, collectionName),
+    orderBy(field, "desc"),
+    limit(1)
+  );
+  const lastDoc = await getDocs(q);
+  if (lastDoc.docs.length === 0) return 0;
+  const lastNum = lastDoc.docs[0].data()[field];
+  return lastNum;
+}
+
+function getQuery(collectionName, queryOption) {
+  const { conditions = [], orderBys = [], limits } = queryOption;
+  const collect = getCollection(collectionName);
+  let q = query(collect);
+
+  // where 조건
+  conditions.forEach((condition) => {
+    q = query(q, where(condition.field, condition.operator, condition.value));
+  });
+
+  // orderBy 조건
+  orderBys.forEach((order) => {
+    q = query(q, orderBy(order.field, order.direction || "asc"));
+  });
+
+  // limit 조건
+  q = query(q, limit(limits));
+
+  return q;
+}
+
+export async function getDatas(collectionName, queryOptions) {
+  const q = getQuery(collectionName, queryOptions);
+  const snapshot = await getDocs(q);
+  const docs = snapshot.docs;
+  const resultData = docs.map((doc) => ({ ...doc.data(), docId: doc.id }));
+  return resultData;
+}
--- react/14-shop-app/src/pages/HomePage/card-list/CardList.js
+++ react/14-shop-app/src/pages/HomePage/card-list/CardList.js
@@ -1,24 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./CardList.module.scss";
 import CardItem from "./card-item/CardItem";
-
-const products = [
-  {
-    id: 1,
-    title: "가방",
-    price: 109.95,
-    description:
-      "숲에서 하루종일 걷고 난리쳐도 좋은 가방. 15인치가 넘는 노트북도 들어감",
-    category: "Men's clothing",
-    image: "/gucci.jfif",
-    rating: {
-      rate: 3.9,
-      count: 120,
-    },
-  },
-];
+import { useDispatch, useSelector } from "react-redux";
+import { fetchProducts } from "../../../store/products/productsSlice";
 
 function CardList() {
+  const dispatch = useDispatch();
+  const { products } = useSelector((state) => state.productsSlice);
+  const category = "Electronics";
+
+  // ※ dependency list([])를 넣어야 됨 ※
+  useEffect(() => {
+    const queryOptions = {
+      conditions: [
+        {
+          field: "category",
+          operator: category ? "==" : ">=",
+          value: category.toLowerCase(),
+        },
+      ],
+    };
+    dispatch(fetchProducts({ collectionName: "shop", queryOptions }));
+  }, [category]);
+
   return (
     <ul className={styles.card_list}>
       {products.map((product) => {
