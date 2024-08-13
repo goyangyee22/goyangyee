722cca08759761482ec08b40fc5c14e11ca05f01
yooeuddeum <48862667+goyangyee22@users.noreply.github.com>
2024-08-13 17:48:04 +0900

    Update CategoryTab.js

--- react/14-shop-app/src/pages/HomePage/filter-category/category-tab/CategoryTab.js
+++ react/14-shop-app/src/pages/HomePage/filter-category/category-tab/CategoryTab.js
@@ -1,11 +1,11 @@
 import React from "react";
 import styles from "./CategoryTab.module.scss";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setActiveCategory } from "../../../../store/categories/categoriesSlice";
 
 function CategoryTab({ text, categoryName }) {
   const dispatch = useDispatch();
-  const category = "";
+  const category = useSelector((state) => state.categoriesSlice);
   return (
     <button
       className={
