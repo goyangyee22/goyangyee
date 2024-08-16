d21b598d0b485e0cc574827b3d36dde322c946f4
yooeuddeum <48862667+goyangyee22@users.noreply.github.com>
2024-08-16 17:29:12 +0900

    Update api.js

--- react/14-shop-app/src/api.js
+++ react/14-shop-app/src/api.js
@@ -5,6 +5,8 @@ import {
 } from "./utils/firebaseTranslate";
 
 const API_KEY = "AIzaSyBP0BMK34Pmmdwt6Lp_hYIvea2rMCiC5O0";
+const AUTH_TOKEN =
+  "eyJhbGciOiJSUzI1NiIsImtpZCI6ImQ0MjY5YTE3MzBlNTA3MTllNmIxNjA2ZTQyYzNhYjMyYjEyODA0NDkiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vc2hvcC1hcHAtMzBiZGEiLCJhdWQiOiJzaG9wLWFwcC0zMGJkYSIsImF1dGhfdGltZSI6MTcyMzc5NjY0MiwidXNlcl9pZCI6IjZYeHJmNEFncmplNEVSdFozdWhQdW43Z3g1TzIiLCJzdWIiOiI2WHhyZjRBZ3JqZTRFUnRaM3VoUHVuN2d4NU8yIiwiaWF0IjoxNzIzNzk2NjQyLCJleHAiOjE3MjM4MDAyNDIsImVtYWlsIjoidGVzdHVzZXIxQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJlbWFpbCI6WyJ0ZXN0dXNlcjFAZ21haWwuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoicGFzc3dvcmQifX0.QOEdEbe0dZR4m2uqk4DOh1l9sAZBjzxDbDOEF29QzURInLNefyAtAGFn5D98NTnxQ7I7uo2ISoFF_j0N1wfHddOgKL--GwDtPdu-zVrkiV3tUp7Kers6d-_qvkpN4cXsqHixpU3HYzNlBTNJYK4rV_uTk-BcgfaDbHuO_iaEL36wfDUNF9DOEJcEhVEgEBWd5NKk0sPxeNyD2hyc4PsNef2laBk76H4FVsI6Uf1Q0AzdPKIa1Wl6UTSVPYzrt94NLysZavNPIWfU641sD7lq4G-e5iJ_pC3UskSp4e3xGyi67O7rn2OSYTs42K8GxvhSeUVoXh-Zbqy-xK12kvkFQA";
 
 const BASE_URL =
   "https://firestore.googleapis.com/v1/projects/shop-app-30bda/databases/(default)/documents";
@@ -93,7 +95,11 @@ export async function deleteDatasRestBatch(url, dataArr) {
     const response = await api.post(
       ":batchWrite",
       { writes: requests },
-      { params: { key: API_KEY } }
+      {
+        headers: {
+          Authorization: `Bearer ${AUTH_TOKEN}`,
+        },
+      }
     );
     return true;
   } catch (error) {
